80d9436efa3b8babd0ca4115edb73fa553c2c7fe
Fran Bejarano Escano <147075385+FranEscano@users.noreply.github.com>
2023-12-04 08:45:23 +0000

    delete env variable

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,7 @@ const fs = require('fs');
 const cypress = require("cypress");
 
 module.exports = defineConfig({
-  env:{
-    ELECTRON_EXTRA_LAUNCH_ARGS: --disable-gpu
-  },
-  e2e: {
+    e2e: {
     downloadsFolder: "cypress/downloads",
     setupNodeEvents(on, config) {
       // implement node event listeners here
